0ed07368da8377d0da9685859ff4330fbebc1601
adrianartale <23aartale@athenian.org>
2021-10-28 10:59:07 -0700

    finished

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,33 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
-    
+    public Dog(String name, int age, String breed){
+        super(name,age);
+        this.breed = breed;
+    }
 
+    public Dog(){
+        this.breed = "pug";
+    }
     // makeNoise() method
-
+    public void makeNoise(){
+        System.out.println("ruf ruf ruf");
+    }
 
     // toString method
-
+    public String toString(){
+        return getName() + ", " + getAge() + " " + breed;
+    }
 
     // Getter
-
+    public String getBreed(){
+        return breed;
+    }
 
     // Setter
+    public void setBreed(String breed){
+        this.breed = breed;
+    }
 }
